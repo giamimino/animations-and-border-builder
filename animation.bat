31af285bae56389b6afbdb72b1ea52d648bb57a7
შოთა მიმინოშვილი <miminoshvili.shota1@students.gov.ge>
2024-07-29 18:35:17 +0400

    Done

---
+++ animation.bat
@@ -0,0 +1,124 @@
+::ia ASCII Animation
+@echo OFF
+Title Earth ASCII Animation
+color 0A
+echo.
+:load
+ping localhost -n 1 >nul
+cls
+echo      --------------------------------------
+ping localhost -n 1 >nul
+cls
+echo      #-------------------------------------
+ping localhost -n 1 >nul
+cls
+echo      ##------------------------------------
+ping localhost -n 1 >nul
+cls
+echo      ###-----------------------------------
+ping localhost -n 1 >nul
+cls
+echo      ####----------------------------------
+ping localhost -n 1 >nul
+cls
+echo      #####---------------------------------
+ping localhost -n 1 >nul
+cls
+echo      ######--------------------------------
+ping localhost -n 1 >nul
+cls
+echo      #######-------------------------------
+ping localhost -n 1 >nul
+cls
+echo      ########------------------------------
+ping localhost -n 3 >nul
+cls
+echo      #########-----------------------------
+ping localhost -n 1 >nul
+cls
+echo      ##########----------------------------
+ping localhost -n 1 >nul
+cls
+echo      ###########---------------------------
+ping localhost -n 1 >nul
+cls
+echo      ############--------------------------
+ping localhost -n 1 >nul
+cls
+echo      #############-------------------------
+ping localhost -n 1 >nul
+cls
+echo      ##############------------------------
+ping localhost -n 1 >nul
+cls
+echo      ###############-----------------------
+ping localhost -n 1 >nul
+cls
+echo      ################----------------------
+ping localhost -n 1 >nul
+cls
+echo      #################---------------------
+ping localhost -n 4 >nul
+cls
+echo      ##################--------------------
+ping localhost -n 2 >nul
+cls
+echo      ###################-------------------
+ping localhost -n 1 >nul
+cls
+echo      ####################------------------
+ping localhost -n 1 >nul
+cls
+echo      #####################-----------------
+ping localhost -n 1 >nul
+cls
+echo      ######################----------------
+ping localhost -n 3 >nul
+cls
+echo      #######################---------------
+ping localhost -n 3 >nul
+cls
+echo      ########################--------------
+ping localhost -n 2 >nul
+cls
+echo      #########################-------------
+ping localhost -n 1 >nul
+cls
+echo      ##########################------------
+ping localhost -n 1 >nul
+cls
+echo      ###########################-----------
+ping localhost -n 1 >nul
+cls
+echo      ############################----------
+ping localhost -n 1 >nul
+cls
+echo      #############################---------
+ping localhost -n 1 >nul
+cls
+echo      ##############################--------
+ping localhost -n 1 >nul
+cls
+echo      ###############################-------
+ping localhost -n 1 >nul
+cls
+echo      ################################------
+ping localhost -n 1 >nul
+cls
+echo      #################################-----
+ping localhost -n 1 >nul
+cls
+echo      ##################################----
+ping localhost -n 1 >nul
+cls
+echo      ###################################---
+ping localhost -n 1 >nul
+cls
+echo      ####################################--
+ping localhost -n 1 >nul
+cls
+echo      #####################################-
+ping localhost -n 1 >nul
+cls
+echo      ######################################
+goto :load
---
+++ hello.bat
@@ -0,0 +1,124 @@
+::ia ASCII Animation
+@echo OFF
+Title Earth ASCII Animation
+color 0A
+echo.
+:hello
+ping localhost -n 1 >nul
+cls
+echo O
+ping localhost -n 1 >nul
+cls
+echo LO
+ping localhost -n 1 >nul
+cls
+echo LLO
+ping localhost -n 1 >nul
+cls
+echo ELLO
+ping localhost -n 1 >nul
+cls
+echo HELLO
+ping localhost -n 1 >nul
+cls
+echo    HELLO
+ping localhost -n 1 >nul
+cls
+echo     HELLO
+ping localhost -n 1 >nul
+cls
+echo      HELLO
+ping localhost -n 1 >nul
+cls
+echo       HELLO
+ping localhost -n 1 >nul
+cls
+echo        HELLO
+ping localhost -n 1 >nul
+cls
+echo          HELLO
+ping localhost -n 1 >nul
+cls
+echo             HELLO
+ping localhost -n 1 >nul
+cls
+echo              HELLO
+ping localhost -n 1 >nul
+cls
+echo                 HELLO
+ping localhost -n 1 >nul
+cls
+echo                    HELLO
+ping localhost -n 1 >nul
+cls
+echo                          HELLO
+ping localhost -n 1 >nul
+cls
+echo                               HELLO
+ping localhost -n 1 >nul
+cls
+echo                                            HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                           HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                        HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                 HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                          HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                               HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                 HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                  HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                   HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                    HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                     HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                      HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                       HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                        HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                         HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                          HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                           HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                            HELLO
+ping localhost -n 1 >nul
+cls
+echo                                                                                                             HELL
+ping localhost -n 1 >nul
+cls
+echo                                                                                                              HEL
+ping localhost -n 1 >nul
+cls
+echo                                                                                                               HE
+ping localhost -n 1 >nul
+cls
+echo                                                                                                                H                                                                                               
+goto :hello
